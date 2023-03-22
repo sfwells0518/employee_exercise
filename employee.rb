377adf75a67f74c222ac026d1e5d00a07733a53d
Sam Wells <sfwells0518@gmail.com>
2023-03-22 12:02:07 -0500

    add class titled  item1

--- employee.rb
+++ employee.rb
@@ -17,13 +17,52 @@
 
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[3]} per year."
 
-employee1 = { "first_name" => "Majora", "last_name" => "Carter", "active" => true, "salary" => 80000 }
-p "#{employee1["first_name"]} #{employee1["last_name"]} makes $#{employee1["salary"]} per year."
+# employee1 = { "first_name" => "Majora", "last_name" => "Carter", "active" => true, "salary" => 80000 }
+# p "#{employee1["first_name"]} #{employee1["last_name"]} makes $#{employee1["salary"]} per year."
 
-employee2 = { :first_name => "Danilo", :last_name => "Campos", :active => false, :salary => 80_000 }
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# employee2 = { :first_name => "Danilo", :last_name => "Campos", :active => false, :salary => 80_000 }
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
-employee2_fancy = { first_name: "Danilo", last_name: "Campos", active: false, salary: 80_000 }
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# employee2_fancy = { first_name: "Danilo", last_name: "Campos", active: false, salary: 80_000 }
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
-p employee2 == employee2_fancy
+# p employee2 == employee2_fancy
+
+# print out Majora Carter makes 80,000 per year
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_active, input_salary)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @active = input_active
+    @salary = input_salary
+  end
+
+  def  print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+
+  def first_name
+   "Majora"
+  end
+
+  def last_name
+   "Carter"
+  end
+
+end 
+
+# employee1 = Employee.new("Majora", "Carter", true, 80_000)
+# employee1.print_info
+
+employee2 = Employee.new("Majora", "Carter", true, 80_000)
+employee2.first_name
+p employee2.first_name
+
+employee3 = Employee.new("Majora", "Carter", true, 80_000)
+employee3.last_name
+p employee3.last_name
+
+
+
+
---
+++ loops.rb
@@ -0,0 +1,39 @@
+# day 1 exercise #1
+
+# print out each element in an array
+
+# fruits = ["pineapple", "pear", "apple", "blueberry"]
+
+# i = 0
+
+# while loops #
+
+# while i < fruits.length
+#     p fruits[i]
+#     i = i +1
+# end
+
+################
+
+# each loops #
+
+# fruits.each do |fruit|
+#   p fruit
+# end
+
+################
+
+# day 1 exercise #2
+
+# numbers = [5,2,6,1]
+# sum = 0
+
+# numbers.each do |number|
+#     sum += number
+
+# end
+# p sum
+
+################
+
+# day 2 exercise #
