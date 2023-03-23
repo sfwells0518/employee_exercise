37f3bbcea76da90d0d895a96fddfab47e0f97635
Sam Wells <sfwells0518@gmail.com>
2023-03-23 13:28:30 -0500

    make methods for giving raises and firing employees

--- manager.rb
+++ manager.rb
@@ -60,6 +60,10 @@
 # manager1.print_info
 # manager1.send_report
 
+##############
+
+# Create a method in the Manager class called give_all_raises that loops through each of the manager’s employees and gives them a raise (using the give_annual_raise method). Demonstrate how it works.
+
 class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
@@ -72,7 +76,7 @@ class Employee
   end
 
   def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+    puts "#{@first_name} #{@last_name} makes $#{@salary} a year."
   end
 
   def give_annual_raise
@@ -82,8 +86,8 @@ end
 
 employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
 employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
+# employee1.print_info
+# employee2.print_info
 
 class Manager < Employee
   def initialize(input_options)
@@ -96,8 +100,53 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+    # each loop below:
+
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+    # while loop below:
+
+    # i = 0
+    # while i < @employees.length
+    #   @employees[i].give_annual_raise
+    #   i += 1
+    # end
+
+    # below are not dynamic or flexible:
+    # p @employees[0].give_annual_raise
+    # p @employees[1].give_annual_raise
+    p "giving all raises"
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
 end
 
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
-manager.print_info
-manager.send_report
+manager1 = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
+manager1.print_info
+# manager gives all raises
+manager1.give_all_raises
+p manager1
+# fire all employees:
+# manager1.fire_all_employees
+# p manager1
+
+# employee1.give_annual_raise
+# employee2.give_annual_raise
+
+# inputs outputs
+# inputs = manager, employees
+# outputs employees but have higher salaries
+# know what salaries they would have with raise (majora 84k, danilo 94.5k)
+
+### Plan of attack for solution:
+
+# make method called give_all_raises
+# inside that method, find all employees (danilo, majora)
+# once i have a particular employee, then give that employee a raise
